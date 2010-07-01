0e5059733af67a883dadd6d24d41ce8cd1df5b0f
Deniz Yuret <denizyuret@gmail.com>
2010-07-01 14:40:33 +0000

    Added some phonetic flags.

--- src/tdk2dict.pl
+++ src/tdk2dict.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl -w -CSD
 # Generates a lexc format dictionary file from maddeler.tab and anlams.tab from tdk
 # See README for the column descriptions of these files.  The important ones are:
 # maddeler[2,3]: WORD_ID, altsay (primary key, 83415 unique)
@@ -9,6 +9,8 @@
 
 
 use strict;
+use utf8;
+
 my %seen;
 
 my %POS = (
@@ -49,10 +51,9 @@ while(<A>) {
 	warn "Warning: $key not found in maddeler.tab\n";
 	next;
     }
-    my $word = $m->[6];		# maddebas
     for my $i (5 .. 8) {	# kelimeturu1-4
 	if (defined $POS{$a[$i]}) {
-	    output($word, $POS{$a[$i]});
+	    output($m, \@a, $i);
 	} elsif ($a[$i] eq '0' or $a[$i] eq '\N') {
 	    # do nothing
 	} else {
@@ -63,9 +64,93 @@ while(<A>) {
 close(A);
 
 sub output {
-    my ($word, $pos) = @_;
+    my ($m, $a, $i) = @_;
+    my $word = $m->[6];		# maddebas
+    my $wpos = $POS{$a->[$i]};
+
     return if $word =~ / /;	# skip multiwords
-    $word =~ s/m[ae]k$// if $pos eq 'Verb'; # get rid of mak/mek
-    my $str = "$word\t+$pos;\n";
-    print $str unless $seen{$str}++;
+    $word =~ s/m[ae]k$// if $wpos eq 'Verb'; # get rid of mak/mek
+
+#    $wpos .= '+Prop' if ($wpos eq 'Noun' and $m->[11] == 1); # ozel
+    $wpos .= '^PL'   if ($wpos eq 'Noun' and $m->[10] == 1); # cogul
+
+    my $ek = $m->[9];		# onek
+    $ek =~ s/[- ]//g;
+    for my $ek1 (split(/,/, $ek)) {
+	my $flag = onek($word, $ek1);
+	my $str = "$word$flag\t+$wpos;\n";
+	print $str unless $seen{$str}++;
+    }
+}
+
+sub oneof { my $x = shift; ($_ eq $x) && return 1 for @_; }
+
+sub onek {
+    my ($word, $ek) = @_;
+    my $flag = '';
+
+    return $flag if $ek eq '\N';
+
+    # Verb aorist flags
+
+    if (oneof($ek, 'dar', 'der')) {
+	$flag = '^CV^AR';
+    } elsif (oneof($ek, 'ar', 'er', 'r')) {
+	$flag = '^AR';
+    } elsif ($ek =~ /^[ıiuü]r$/) {
+	# do nothing
+
+    } else {
+
+	my $lastchar = substr($word, -1);
+	my ($lastvowel) = ($word =~ /([aeıioöuüâîû])[^aeıioöuüâîû]*$/);
+	my ($lastconspair, $lastconspair2) = ('', '');
+	$lastconspair = $word;
+	$lastconspair =~ s/[aeıioöuüâîû]//g;
+	$lastconspair = substr($lastconspair, -2);
+	$lastconspair = '' unless length($lastconspair) == 2;
+
+	if ($lastconspair and $lastconspair =~ /[pçtkg]$/) {
+	    $lastconspair2 = $lastconspair;
+	    if ($lastconspair2) {
+		$lastconspair2 =~ s/p$/b/;
+		$lastconspair2 =~ s/ç$/c/;
+		$lastconspair2 =~ s/t$/d/;
+		$lastconspair2 =~ s/[kg]$/ğ/;
+	    }
+	}
+
+	# Noun flags
+	
+	if ($ek =~ /^$lastchar$lastchar/) {
+	    $flag .= '^CD';
+	}
+	if ($lastchar =~ /^[pçtkg]$/ and $ek =~ /[bcdgğ][ıiuü]$/) {
+	    $flag .= '^CV';
+	}
+	if ($lastconspair and $ek =~ /^$lastconspair/) {
+	    $flag .= '^VD';
+	}
+	elsif ($lastconspair2 and $ek =~ /^$lastconspair2/) {
+	    $flag .= '^VD';
+	}
+	if ($lastvowel =~ /^[aâıouû]$/ and $ek =~ /[iü]$/) {
+	    $flag .= '^VF';
+	}
+	if ($ek eq 'yu') {
+	    $flag .= '^Y';		# rename this yu, check out yi yı in tdk, rethink ne
+	}
+	
+	# Unrecognized
+	
+	if ($flag ne '' or
+	    $ek =~ /^y[ıi]$/ or
+	    $ek =~ /^[pçtk]\'[ıiuü]$/) {
+	    # do nothing
+	} else {
+	    warn "Warning: $word-$ek not a recognized suffix (lastchar=$lastchar,lastvowel=$lastvowel,lastconspair=$lastconspair,lcp2=$lastconspair2,flag=$flag).\n";
+	}
+    }
+    
+    return $flag;
 }
